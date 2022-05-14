65681962860d1a24bd1f337c530221bd13aff7af
damola06 <adedamola.oduro@gmail.com>
2022-05-14 14:43:02 +0100

    added week 4 features

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import sys
+import csv
+
 
 def display_list_with_index(list):
     for index, value in enumerate(list):
         print(index, value)
+
 
 def display_menu_options():
     print('''
@@ -34,7 +37,7 @@ def display_orders_options():
 
 def display_main_menu():
     print(f"Current menu")
-    print(*main_menu, sep="\n")
+    print(*products, sep="\n")
 
 
 def display_courier():
@@ -46,7 +49,8 @@ def display_orders():
     for index, value in enumerate(orders):
         print(value)
 
-def handle_menu_options():
+
+def handle_product_options():
     display_menu_options()
     selected_option = int(input("Select option 1, 2, 3 or 4 to proceed, 0 to quit: "))
 
@@ -55,25 +59,38 @@ def handle_menu_options():
 
     if selected_option == 1:
         display_main_menu()
-        handle_menu_options()
+        handle_product_options()
 
     if selected_option == 2:
-        new_product = input("Enter product name: ")
-        main_menu.append(new_product)
-        handle_menu_options()
+        new_product_name = input("Enter product name: ")
+        new_product_price = input("Enter product price: ")
+
+        product = {
+            "name": new_product_name,
+            "price": new_product_price
+        }
+
+        products.append(product)
+        handle_product_options()
 
     if selected_option == 3:
-        display_list_with_index(main_menu)  # this means calling the function created above
+        display_list_with_index(products)  # this means calling the function created above
         index_to_be_updated = int(input("Select the index of the item you want to update: "))
-        new_product_name = input("Enter new product name: ")
-        main_menu[index_to_be_updated] = new_product_name
-        handle_menu_options()
+
+        product = products[index_to_be_updated]
+        for key, value in product.items():
+            new_value = input(f"Please enter {key}: ")
+            if new_value == "":
+                new_value = value
+            product[key] = new_value
+        products[index_to_be_updated] = product
+        handle_product_options()
 
     if selected_option == 4:
-        display_list_with_index(main_menu)
+        display_list_with_index(products)
         index_to_be_deleted = int(input("Select the index of the item you want to delete: "))
-        main_menu.pop(index_to_be_deleted)
-        handle_menu_options()
+        products.pop(index_to_be_deleted)
+        handle_product_options()
 
 
 def handle_courier_options():
@@ -88,19 +105,33 @@ def handle_courier_options():
         handle_courier_options()
 
     if selected_option == 2:
-        new_courier = input("Enter courier name: ")
+        new_courier_name = input("Enter courier name: ")
+        new_courier_phone = input("Enter courier phone: ")
+
+        new_courier = {
+            "name": new_courier_name,
+            "phone": new_courier_phone
+        }
+
         couriers.append(new_courier)
         handle_courier_options()
 
     if selected_option == 3:
-        display_list_with_index(main_menu)  # this means calling the function created above
+        display_list_with_index(couriers)  # this means calling the function created above
         index_to_be_updated = int(input("Select the index of the courier you want to update: "))
-        new_courier_name = input("Enter new courier name: ")
-        couriers[index_to_be_updated] = new_courier_name
+
+        courier = couriers[index_to_be_updated]
+        for key, value in courier.items():
+            new_value = input(f"Please enter {key}: ")
+            if new_value == "":
+                new_value = value
+            courier[key] = new_value
+        couriers[index_to_be_updated] = courier
+
         handle_courier_options()
 
     if selected_option == 4:
-        display_list_with_index(main_menu)
+        display_list_with_index(products)
         index_to_be_deleted = int(input("Select the index of the courier you want to delete: "))
         couriers.pop(index_to_be_deleted)
         handle_courier_options()
@@ -118,6 +149,8 @@ def handle_order_options():
         handle_order_options()
 
     if selected_option == 2:
+        display_list_with_index(products)
+        customer_products = input("Please enter the indexes of the products you want in a comma separated fashion: ")
         customer_name = input("Please enter customer name: ")
         customer_address = input("Please enter customer address: ")
         customer_phone_number = input("Please enter customer phone: ")
@@ -129,13 +162,15 @@ def handle_order_options():
             "customer_address": customer_address,
             "customer_phone": customer_phone_number,
             "courier": selected_courier,
-            "status": "PREPARING"
+            "status": "PREPARING",
+            "items": customer_products
         }
 
         orders.append(order)
         handle_order_options()
 
     if selected_option == 3:
+        # update status of an order
         display_list_with_index(orders)  # this means calling the function created above
         # get index of order to be updated
         index_to_be_updated = int(input("Select the index of the order you want to update status for: "))
@@ -160,10 +195,18 @@ def handle_order_options():
             if key == "status":
                 continue
 
+            if key == "items":
+                display_list_with_index(products)
+                selected_items = input("Please enter the indexes of the products in a comma separated fashion: ")
+                if selected_items != "":
+                    order["items"] = selected_items
+                continue
+
             if key == "courier":
                 display_list_with_index(couriers)
-                selected_courier = int(input("Please select a courier: "))
-                order['courier'] = selected_courier
+                selected_courier = input("Please select a courier: ")
+                if selected_courier != "":
+                    order['courier'] = selected_courier
                 continue
 
             new_value = input(f"Please enter {key}: ")
@@ -181,15 +224,16 @@ def handle_order_options():
 
 
 def show_main_menu():
-    user_input = int(input("Please enter 0 to quit, 1 to manage menu, 2 to manage couriers and 3 to manage orders: "))
+    user_input = int(input("Please enter 0 to quit, 1 to manage products, 2 to manage couriers and 3 to manage orders: "))
 
     if user_input == 0:
-        write_file("data/products.txt", main_menu)
-        write_file("data/couriers.txt", couriers)
+        write_file("data/products.csv", products)
+        write_file("data/couriers.csv", couriers)
+        write_file("data/orders.csv", orders)
         sys.exit()
 
     if user_input == 1:
-        handle_menu_options()  # this calls the function that shows the menu options and handles them
+        handle_product_options()  # this calls the function that shows the menu options and handles them
 
     if user_input == 2:
         handle_courier_options()  # this calls the function that shows the courier options and handles them
@@ -199,24 +243,27 @@ def show_main_menu():
 
 
 def read_file(file_path):
-    fileObj = open(file_path, "r")  # opens the file in read mode
-    words = fileObj.read().splitlines()  # puts the file into a list
-    fileObj.close()
-    return words
+    with open(file_path) as f:
+        result = [{k: v for k, v in row.items()}
+             for row in csv.DictReader(f, skipinitialspace=True)]
+
+        return result
 
 
 def write_file(file_path, content):
-    joined_string = "\n".join(content)
-    file = open(file_path, 'w')
-    file.write(joined_string)
-    file.close()
+    keys = content[0].keys()
+
+    with open(file_path, 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(content)
 
 
-main_menu = read_file("data/products.txt")
+products = read_file("data/products.csv")
 
-couriers = read_file("data/couriers.txt")
+couriers = read_file("data/couriers.csv")
 
-orders = []
+orders = read_file("data/orders.csv")
 
 order_statuses = ["PREPARING", "OUT_FOR_DELIVERY", "DELIVERED"]
 
